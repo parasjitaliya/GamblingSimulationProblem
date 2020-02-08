55adbdd02fea87d0b52d6f874c3e80c7e585b76b
paras <parasjitaliya@gmail.com>
2020-02-08 17:34:49 +0530

    Added:condition for resignand function

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,15 +3,25 @@ echo "----welcome to gambling Simulation----"
 #initialize variable
 stake_for_day=100
 bet=1
-#check win or lose 
-check=$((RANDOM%2))
-if [ $check -eq 1 ]
-then
-	echo "WON"
-	amount=$(($STAKE_FOR_DAY+$BET_PER_GAME))
-else
-	echo "LOST"
-	amount=$(($STAKE_FOR_DAY-$BET_PER_GAME))
-fi        
+#calculate resignpercent
+function resignpercent() {
+	echo "$(($stake_for_day*50/100))"
+}
+val="$(resignpercent)"
+wonResign=$(($stake_for_day+$val))
+loseResign=$(($stake_for_day-$val))
+
+while [[ $wonResign -gt $stake_for_day  && $loseResign -lt $stake_for_day ]]
+do
+	#check win or lose 
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		echo "WON"
+		amount=$(($stake_for_day+$Bet))
+	else
+		echo "LOST"
+		amount=$(($stake_for_day-$Bet))
+	fi
+done        
 }
 
