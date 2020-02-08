e9f0a4b52953d09eb67446e6dbb6bc85d658c20b
paras <parasjitaliya@gmail.com>
2020-02-08 17:17:24 +0530

    Added:Check Won or lost

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,3 +3,15 @@ echo "----welcome to gambling Simulation----"
 #initialize variable
 stake_for_day=100
 bet=1
+
+check=$((RANDOM%2))
+if [ $check -eq 1 ]
+then
+	echo "WON"
+	amount=$(($STAKE_FOR_DAY+$BET_PER_GAME))
+else
+	echo "LOST"
+	amount=$(($STAKE_FOR_DAY-$BET_PER_GAME))
+fi        
+}
+
